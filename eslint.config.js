eea70a1eaf71b74420dd63151160a68e4ee82109
Toil <62353659+ilyhalight@users.noreply.github.com>
2024-11-25 14:55:48 +0300

    migrated to eslint flat config

---
+++ eslint.config.js
@@ -0,0 +1,27 @@
+import js from "@eslint/js";
+import oxlint from "eslint-plugin-oxlint";
+import tseslint from "typescript-eslint";
+
+export default tseslint.config(
+  {
+    ignores: ["dist/*", "docs/*", "**/*.d.ts", "scripts/*"],
+  },
+  js.configs.recommended,
+  ...tseslint.configs.recommendedTypeChecked,
+  ...tseslint.configs.stylisticTypeChecked,
+  {
+    rules: {
+      "@typescript-eslint/no-explicit-any": 0,
+      "@typescript-eslint/consistent-type-definitions": 0,
+    },
+    languageOptions: {
+      ecmaVersion: "latest",
+      sourceType: "module",
+      parserOptions: {
+        project: true,
+        tsconfigDirName: import.meta.dirname,
+      },
+    },
+  },
+  oxlint.configs["flat/recommended"], // oxlint should be the last one
+);
